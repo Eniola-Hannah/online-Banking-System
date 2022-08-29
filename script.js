a63f5a18fce9500940866275e50ab654fce63f61
Eniola Hannah <heniola395@gmail.com>
2022-08-29 11:44:15 +0100

    still working on my sign up page

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+customerDetails = []
 const signUp = () =>{
-    if
+    let userDetails={
+            firstName : firstname.value,
+            lastName : lastname.value,
+            email : userEmail.value,
+            password : userPassword.value,
+            phoneNumber : phonenumber.value
+        }
+    // customerDetails =
+    // console.log(userDetails)
+    if(userDetails.firstName == firstname.value && userDetails.lastName == lastname.value && userDetails.email == userEmail.value && userDetails.password == userPassword.value && userDetails.phoneNumber == phonenumber.value){
+        customerDetails=userDetails.push()
+        firstname.value =""
+        lastname.value =""
+        userEmail.value =""
+        userPassword.value =""
+        phonenumber.value =""
+        // window.location.href = "sign.html"
+    }
+    else{
+        console.log("Fill the Required Spaces")
+    }
 }
